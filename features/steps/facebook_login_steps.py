f4ddf6d1d688aeaebafacd037df1d25b4ca6229d
Niranjan <niranjan1111@gmail.com>
2023-03-19 22:30:23 +0530

    Added requirement.txt

--- features/steps/facebook_login_steps.py
+++ features/steps/facebook_login_steps.py
@@ -6,27 +6,29 @@ from selenium.webdriver.support import expected_conditions as EC
 from Helper.SeleniumHelper import SeleniumHelper
 from locators import locators
 from TestData import login_details
+from loggi.mylog import logging
 
 import time
 log_url = r"https://www.facebook.com/"
 
 @given(u'User on the login page')
 def step_impl(context):
+    logging.info("Opening the website")
     SeleniumHelper(context.driver).open_page(log_url)
-    print("face book page is opend")
+    logging.info("Facebook page opened")
+
+
 
 
 
 @when(u'user input incorrect credentials')
 def step_impl(context):
+    logging.info("Entering Username")
     SeleniumHelper(context.driver).text_box_entry(locators.input_field_login, login_details.correct_login_id )
+    logging.info("Entering password")
     SeleniumHelper(context.driver).text_box_entry(locators.input_field_password, login_details.incorrect_login_password)
+    logging.info("Clicking on login button")
     SeleniumHelper(context.driver).click_button(locators.login_button)
-    time.sleep(10)
-    # context.driver.find_element(By.XPATH, "//input[@name = 'email']").send_keys("niranjan1111@gmail.com")
-    # context.driver.find_element(By.XPATH, "//input[@name = 'pass']").send_keys("niranjan1111@gmail.com")
-    # context.driver.find_element(By.XPATH, "//button[@name = 'login']").click()
-
 
 @then(u'Error message will come')
 def step_impl(context):
--- loggi/mylog.py
+++ loggi/mylog.py
@@ -1,19 +1,19 @@
-import logging as lg
+import logging
 import os
 from datetime import datetime
 
 # log file name
 LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
 
-#Log file dir
-LOG_FILE_DIR = os.path.join(os.getcwd())
-print(LOG_FILE_DIR)
+# Log file dir
+LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
 
-#Log file path
-LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
-
-lg.basicConfig(
+# Log file path
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIR, LOG_FILE_NAME)
+os.makedirs(LOG_FILE_DIR,exist_ok=True)
+logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format = "[%(asctime)s] % (lineno)d %(name)s - %(levelname)s- %(message)s",
-    level= lg.INFO
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+
+    level=logging.INFO,
 )
